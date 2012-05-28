30ccb2c8364701e3335ce112736df3c992f57a7d
Johannes Mitlmeier <github@spam.slzm.de>
2012-05-28 23:46:09 +0200

    Großes Update (Unzip, Split, Merge)

--- getgpxosm/BoundingBoxSaxParser.py
+++ getgpxosm/BoundingBoxSaxParser.py
@@ -1,49 +1,56 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
 # 2012, Johannes Mitlmeier, GPL
 # Benötigt wohl Python 2.7 unter Linux
 
 
-import subprocess, os
-
+import subprocess, os, re, codecs
 from xml.sax import make_parser, handler
 
 class BoundingBoxSaxParser(handler.ContentHandler):
-    def __init__(self, folder, my_id, latlon):
-        self.folder = folder
-        self.my_id = my_id
+    def __init__(self, latlon):
         self.latlon = [float(i) for i in latlon[0].split(',')]
-        self.file_counter = 0
-        self.out_doc = None
-        self.level = 0
-        self.point_counter = 0
-
         self.open_tags = set()
+        self.open_source_tags = set()
         self.active = False
 
+        # Einstellungen
+        self.filter_urls = False
+        self.extra_border = 0.0
+
+
+    def set_file(self, folder, filename):
+        self.folder = folder
+        self.filename = filename        
+        self.out_doc = None
+
+        self.file_counter = 0
+        self.point_counter = 0
+        self.level = 0
+
+
     def close_file(self):
+        print('Trying to close file')
         if self.out_doc is None:
             return
         # trk trkseg trkpt    
-        if 'trkseg' in self.open_tags:
-            self.closeTag('trkseg')
-        if 'trk' in self.open_tags:
-            self.closeTag('trk')
+        self.closeTag('trkseg')
+        self.closeTag('trk')
         self.out_doc.write('\n</gpx>')
         self.out_doc.close()
         self.out_doc = None
 
-        print '%d Punkte' % self.point_counter
+        print('%d Punkte' % self.point_counter)
         # leer?
         if self.point_counter == 0:
-            os.remove(self.filename)
+            os.remove(self.gen_filename)
 
     def next_file(self):
         self.file_counter += 1
-        self.filename = '%s/%s-%04d.gpx' % (self.folder, self.my_id, self.file_counter)
-        self.out_doc = open(self.filename, 'wb')
-        print 'Schreibe Track in Datei %s' % (self.filename)
+        self.gen_filename = '%s/%s-%04d.gpx' % (self.folder, self.filename, self.file_counter)
+        self.out_doc = codecs.open(self.gen_filename, 'wb', 'utf-8')
+        print('Schreibe Track in Datei %s' % (self.gen_filename))
         self.out_doc.write("""<?xml version="1.0" encoding="UTF-8"?>
 <gpx version="1.1" creator="getgpxosm aus Daten von OpenStreetMap">
         """)
@@ -51,22 +58,22 @@ class BoundingBoxSaxParser(handler.ContentHandler):
         self.point_counter = 0
 
     def is_good_point(self, lat, lon):
-        if lat < self.latlon[0]:
+        if lat < self.latlon[0] + self.extra_border:
             return False
-        if lat > self.latlon[2]:
+        if lat > self.latlon[2] - self.extra_border:
             return False
-        if lon < self.latlon[1]:
+        if lon < self.latlon[1] + self.extra_border:
             return False
-        if lon > self.latlon[3]: 
+        if lon > self.latlon[3] - self.extra_border: 
             return False
         return True
         
     def openTag(self, name, attrs):
         if self.out_doc is None:
             return
-        self.out_doc.write('%s<%s' % ('\t'*self.level, name))
+        self.out_doc.write('\n%s<%s' % ('\t'*self.level, name))
         if not attrs is None:
-            for attr in attrs.keys():
+            for attr in list(attrs.keys()):
                 self.out_doc.write(' %s="%s"' % (attr, attrs[attr]))
         self.out_doc.write('>')
         self.level += 1
@@ -75,35 +82,52 @@ class BoundingBoxSaxParser(handler.ContentHandler):
     def closeTag(self, name):
         if self.out_doc is None:
             return
-        self.out_doc.write('</%s>' % (name))
-        self.level -= 1
         try:
             self.open_tags.remove(name)
+            self.out_doc.write('</%s>' % (name))
+            self.level -= 1
         except KeyError:
             pass
 
     def startElement(self, name, attrs):
+        self.open_source_tags.add(name)
+
+        if 'cmt' in self.open_source_tags:
+            return
+        if name == 'url' and self.filter_urls:
+            self.active = False
+            return
         if name == 'trk':
             self.next_file()
             self.active = True
         elif name == 'trkseg':
             return
         elif name == 'trkpt':
-            if self.is_good_point(float(attrs['lat']), float(attrs['lon'])):
-                self.point_counter += 1                
-                # ggf. Segment öffnen (weil unterbrochen)
-                if not 'trkseg' in self.open_tags:
-                    self.openTag('trkseg', None)
-                self.active = True
-            else:
-                if 'trkseg' in self.open_tags:
-                    self.closeTag('trkseg')
-                self.active = False # es darf nichts ausgegeben werden, der Punkt ist raus
+            if not self.filter_urls or self.active:
+                if self.is_good_point(float(attrs['lat']), float(attrs['lon'])):
+                    self.point_counter += 1                
+                    # ggf. Segment öffnen (weil unterbrochen)
+                    if not 'trkseg' in self.open_tags:
+                        self.openTag('trkseg', None)
+                    self.active = True
+                else:
+                    if 'trkseg' in self.open_tags:
+                        self.closeTag('trkseg')
+                    self.active = False # es darf nichts ausgegeben werden, der Punkt ist raus
 
         if self.active:
             self.openTag(name, attrs)
 
     def endElement(self, name):
+        try:
+            self.open_source_tags.remove(name)
+        except KeyError:
+            pass
+
+        if 'cmt' in self.open_source_tags:
+            if not name == 'cmt':
+                return
+                
         if name == 'trk':
             self.active = False
             self.close_file()
@@ -111,13 +135,19 @@ class BoundingBoxSaxParser(handler.ContentHandler):
             if 'trkseg' in self.open_tags:
                 self.closeTag('trkseg')
                 return           
+        elif name == 'trkpt':
+            self.active = True            
 
         if self.active:
             self.closeTag(name)
 
     def characters(self, content):
+        if 'cmt' in self.open_source_tags:
+            return
+        if re.match('^\s*$', str(content)):
+            return
         if self.active:
-            self.out_doc.write(content.encode('utf8'))
+            self.out_doc.write(content)
 
     def endDocument(self):
         self.close_file()
--- getgpxosm/getgpxosm.py
+++ getgpxosm/getgpxosm.py
@@ -1,12 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
 # 2012, Johannes Mitlmeier, GPL
 # Benötigt wohl Python 2.7 unter Linux, gpsbabel zum Mergen
 
 
-import urllib, sys, os, re, argparse, subprocess, gc
+import urllib.request, urllib.parse, urllib.error, sys, os, re, argparse, subprocess, gc, codecs
 from xml.sax import make_parser, handler
+from xml.sax._exceptions import SAXParseException
 from BoundingBoxSaxParser import BoundingBoxSaxParser
 gc.enable()
 
@@ -15,7 +16,7 @@ dl_cache = set()
 def download(url, filename):
     if url + '#' + filename in dl_cache:
         return    
-    urllib.urlretrieve(url, filename)
+    urllib.request.urlretrieve(url, filename)
     dl_cache.add(url + '#' + filename)
 
 
@@ -26,10 +27,12 @@ parser.add_argument('-ll, --latlon', dest='latlon', nargs=1, metavar='BOUNDING_B
 parser.add_argument('-w, --width', dest='width', nargs=1, metavar='FLOAT', default=[0.45], type=float, help='Breite/Höhe der unterteilten Bounding-Boxen')
 parser.add_argument('-d, --download', dest='download', action='store_true', help='Download der Rohdaten aktivieren')
 parser.add_argument('-e, --extract', dest='extract', action='store_true', help='Extraktion und Download der GPX-Dateien aktivieren')
+parser.add_argument('-u, --unzip', dest='unzip', action='store_true', help='Archive mit GPX-Dateien auspacken')
 parser.add_argument('-bb, --bounding-box', dest='boundingbox', action='store_true', help='GPX-Dateien auf Bounding Box (siehe Parameter -ll) beschränken')
+parser.add_argument('-s, --safety-border', dest='safety-border', nargs=1, metavar='FLOAT', default=[0.00], type=float, help='Randabstand zur Bounding-Box, um korrekt zu trennen')
 parser.add_argument('-m, --merge', dest='merge', action='store_true', help='Einzelne GPX-Tracks zu einer Datei bündeln')
 args = parser.parse_args()
-print args
+print(args)
 
 
 
@@ -61,13 +64,13 @@ if args.__dict__['download']:
         while True:
             url = 'http://api.openstreetmap.org/api/0.6/trackpoints?bbox=%s&page=%d' % (tile ,page)
             filename = 'API/page%04d.xml' % output_counter
-            print 'Download Seite %d: %s' % (page, url)
+            print('Download Seite %d: %s' % (page, url))
             download(url, filename)
             page = page + 1
             output_counter = output_counter + 1
             # Dateigröße prüfen
             if os.path.getsize(filename) < 131:
-                print 'Datenseiten beendet. Dateigröße %d (%s).' % (os.path.getsize(filename), open(filename, 'rb').read())
+                print('Datenseiten beendet. Dateigröße %d (%s).' % (os.path.getsize(filename), codecs.open(filename, 'rb', 'utf-8').read()))
                 os.remove(filename)
                 break
 
@@ -79,21 +82,65 @@ if args.__dict__['extract']:
         os.makedirs('GPX')
     folder = os.getcwd() + '/API/'
     files = os.listdir(folder)
+
+    working_dir = os.getcwd()
+    os.chdir(working_dir + '/GPX')
+
     for filename in files:
         if not filename.endswith('.xml'):
             continue
         full_filename = folder + filename
-        print 'Extrahiere Links aus Datei %s' % full_filename
-        results = re.finditer('<url>(.*?(\d+))</url>', open(full_filename, 'r').read())
+        print('Extrahiere Links aus Datei %s' % full_filename)
+        results = re.finditer('<url>(.*?(\d+))</url>', codecs.open(full_filename, 'r', 'utf-8').read())
         for result in results:
             gpx_url = 'http://www.openstreetmap.org/trace/%s/data/' % result.group(2)
-            filename = 'GPX/%s.gpx' % result.group(2)
-            print 'Download GPX-Datei: %s' % gpx_url
+            filename_real = '%s.gpx' % result.group(2)
+            print('Download GPX-Datei: %s' % gpx_url)
+    
             # URLs herunterladen
-            if not os.path.exists(filename): # kein Überschreiben
-                download(gpx_url, filename)
-                # tar.gz- oder gz-Archive auspacken
-                subprocess.call(['tar', '-zxf', filename, 'GPX'], stderr=subprocess.PIPE)
+            retcode = 1
+            if not os.path.exists(filename_real): # kein Überschreiben
+                download(gpx_url, filename_real)
+            
+    os.chdir(working_dir)
+
+
+# Archive auspacken
+if args.__dict__['unzip']:
+    folder = os.getcwd() + '/GPX/'
+    files = os.listdir(folder)
+
+    working_dir = os.getcwd()
+    os.chdir(working_dir + '/GPX')
+
+    for filename in files:
+        filename_packed = filename + '.packed'
+        os.rename(filename, filename + '.packed')
+
+        # zip
+        retcode = subprocess.call(['unzip', '-u', os.getcwd() + '/' + filename_packed], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if retcode == 0:
+            print("%s (ZIP-Archiv)" % filename)
+        if retcode > 0: 
+            # tar.gz
+            retcode = subprocess.call(['tar', '-zxfv', os.getcwd() + '/' + filename_packed], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            if retcode == 0:
+                print("%s (tar.gz-Archiv)" % filename)
+            if retcode > 0:
+                # gz
+                retcode = subprocess.call(['gunzip', '--suffix', '.packed', '--force', '--verbose', os.getcwd() + '/' + filename_packed], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                if retcode == 0:
+                    print("%s (gz-Archiv)" % filename)
+
+        # wurde etwas entpackt?     
+        try:
+            if retcode == 0:
+                os.remove(filename_packed)
+            else:
+                os.rename(filename_packed, filename)
+        except OSError:
+            pass
+    os.chdir(working_dir)
 
 
 
@@ -102,45 +149,71 @@ if args.__dict__['boundingbox']:
     if not os.path.exists('output'):
         os.makedirs('output')
     
-    # API-Dateien lesen
-    folder = os.getcwd() + '/API/'
-    files = os.listdir(folder)
-    for filename in files:
-        pass
-
     # GPX-Dateien lesen
     folder = os.getcwd() + '/GPX/'
     files = os.listdir(folder)
     parser = make_parser()
-    my_parser = BoundingBoxSaxParser(os.getcwd() + '/output', '', args.__dict__['latlon'])
+    my_parser = BoundingBoxSaxParser(args.__dict__['latlon'])
+    parser.setContentHandler(my_parser)
     for filename in files:
-        #continue
         if not filename.endswith('.gpx'):
             continue
-        print 'Verarbeite %s' % filename
+        print('Verarbeite %s' % filename)
         my_id, ext = os.path.splitext(filename)
         full_filename = folder + filename
-        my_parser.my_id = my_id        
-        parser.setContentHandler(my_parser)
+        my_parser.set_file(os.getcwd() + '/output', my_id)
         try:
-            parser.parse(full_filename)
-        except Exception:
-            print 'ERROR: Parse-Fehler'
+            fh = codecs.open(full_filename, 'r', encoding='utf-8', errors='ignore')
+            #print fh
+            parser.parse(fh)
+        except SAXParseException as ex:
+            print('%s: %s' % (type(ex), ex))
+        except (UnicodeEncodeError, UnicodeDecodeError) as ex:
+            print('%s: %s' % (type(ex), ex))
+        except Exception as ex:
+            print('%s: %s' % (type(ex), ex))
+
+        my_parser.close_file()
         gc.collect()
+
+    # API-Dateien lesen
+    folder = os.getcwd() + '/API/'
+    files = os.listdir(folder)
+    my_parser.filter_urls = True
+    my_parser.extra_border = args.__dict__['safety-border'][0]
+    for filename in files:
+        if not filename.endswith('.xml'):
+            continue
+        print('Verarbeite %s' % filename)
+        my_id, ext = os.path.splitext(filename)
+        full_filename = folder + filename
+        my_parser.set_file(os.getcwd() + '/output', my_id)
+        try:
+            parser.parse(codecs.open(full_filename, 'r', 'utf-8'))
+        except Exception as ex:
+            print('%s: %s' % (type(ex), ex))
+
+        my_parser.close_file()
+        gc.collect()
+
+
+
 
 
 # Jens Fischer
 if args.__dict__['merge']:
+    print('Dateien zusammenfügen...')    
+    folder = os.getcwd() + '/output/'
     try:    
-        os.remove(os.getcwd() + '/output/merged.gpx')
+        os.remove(folder + '_merged.gpx')
     except OSError:
         pass
     babelargs = ['gpsbabel', '-i', 'gpx']
-    folder = os.getcwd() + '/output/'
     files = os.listdir(folder)
     for filename in files:
     	babelargs.extend(['-f', folder + filename])
-    babelargs.extend(['-o', 'gpx', '-F', folder + 'merged.gpx'])
+    babelargs.extend(['-o', 'gpx', '-F', folder + '_merged.gpx'])
     subprocess.call(babelargs)
+    print('Fertig')
 
 
